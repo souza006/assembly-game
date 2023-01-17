28846819b3a2abafcee961893d84fb2621197a27
Elvis Souza <souzaelvis33@gmail.com>
2023-01-17 16:02:17 -0300

    feat: space invaders files

---
+++ jogo-1.asm
@@ -0,0 +1,961 @@
+
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4
+	add $9,$9, $24 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 232 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 228 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	
+
+#inimigo3:
+
+	lui $9, 0x1000	
+	addi $9, $9, 1980  
+	add $9,$9, $17
+	
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4
+	add $9,$9, $24 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 232 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 228 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+
+
+
+#inimigo4:
+	lui $9, 0x1000	
+	addi $9, $9, 3608 
+	add $9,$9, $17
+	
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4
+	add $9,$9, $24 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 232 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 228 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+
+#inimigo5:
+
+	lui $9, 0x1000	
+	addi $9, $9,3692  
+	add $9,$9, $17
+	
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4
+	add $9,$9, $24 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 232 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 228 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+
+#inimigo6:
+
+	lui $9, 0x1000	
+	addi $9, $9,3772  
+	add $9,$9, $17
+	
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4
+	add $9,$9, $24 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 232 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	addi $9, $9, 4 
+	add $9,$9, $24
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 228 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x6b0909
+	sw $10, 0($9)
+	add $9,$9, $24
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	
+	jr $31
+	
+
+vidas:
+
+#vida 1
+	lui $9, 0x1000
+	addi $10, $0, 0x03770d      
+	addi $9,$9,13832
+     add $9,$9, $22
+     sw $10,0($9)     
+      addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+
+     addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+      addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+      addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+      addi $9,$9,-264
+     add $9,$9, $25
+     sw $10,0($9)
+     
+     addi $9,$9,-4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     addi $9,$9,8
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     addi $9,$9,-260
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     
+     
+  #vida 2   
+     lui $9, 0x1000
+     addi $10, $0, 0x03770d      
+     addi $9,$9,13860
+     add $9,$9, $22
+     sw $10,0($9)     
+      addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+
+     addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+      addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+      addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+      addi $9,$9,-264
+     add $9,$9, $25
+     sw $10,0($9)
+     
+     addi $9,$9,-4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     addi $9,$9,8
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     addi $9,$9,-260
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     
+#vida 3
+     lui $9, 0x1000
+     addi $10, $0, 0x03770d      
+     addi $9,$9,13888
+     add $9,$9, $22
+     sw $10,0($9)     
+      addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+
+     addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+      addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+      addi $9,$9,4
+     add $9,$9, $22
+     sw $10,0($9)
+      
+      addi $9,$9,-264
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     addi $9,$9,-4
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     addi $9,$9,8
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     addi $9,$9,-260
+     add $9,$9, $22
+     sw $10,0($9)
+     
+     
+     j main
+
+nave:
+	lui $9, 0x1000 
+     	addi $9,$9,11636
+ 	add $9, $12, $9
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x03770d
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 244 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x03770d
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, 236 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0x03770d
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	sw $10, 0($9)
+	addi $9, $9, 4 
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+
+    	jr $31
+
+desenhetiro: 
+	lui $9, 0x1000 
+     	addi $9,$9,11384
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, -256 
+ 
+	addi $10, $0, 0x03770d
+	sw $10, 0($9)
+	addi $9, $9,-256
+	
+	addi $10, $0, 0x03770d
+	sw $10, 0($9)
+	addi $9, $9,-256
+ 
+	addi $10, $0, 0xbabcba
+	sw $10, 0($9)
+	addi $9, $9, -256 	
+	
+	jr $31
+
+verifientrada:
+	lui $12, 0xffff
+	lw  $13, 0($12)
+	beq $13, $0, nave
+
+	addi $12 , $12 , 4
+	lw $13, 0($12)
+	
+	beq $13, 65, moveesquerda #move a nave para a esquerda
+	beq $13, 68, movedireita # move nave para a direita
+	
+	jr $31
+
+moveesquerda:
+	 
+	 beq $11,-112,movedireita
+	 addi $11,$11,-4
+	
+	 jr $31
+	 
+movedireita:
+	 
+	 beq $11,124,moveesquerda
+	 addi $11,$11,4
+	 
+	 jr $31
+
+
+movimento_inimigo:
+
+	beq $13, 1, esquerdinha_movimentinho
+	
+direitinha_movimentinho:
+		beq $17, 24, esquerdinha_movimentinho
+		addi $17, $17, 4
+		addi $13, $0, 0
+		jr $31
+esquerdinha_movimentinho:
+		beq $17, 0, direitinha_movimentinho
+		addi $17, $17, -4
+		addi $13, $0, 1
+		jr $31	
+		
+		
+main: 
+#	jal navefantasma
+	jal verifientrada
+	jal nave
+	jal inimigos
+	jal movimento_inimigo 
+	
+	j main
+
